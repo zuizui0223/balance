c273dc06f044c28489dd228b3d7aa5c334a9477a
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-05 16:17:17 +0900

    Expose middle-world certificate API

--- balance_domain/__init__.py
+++ balance_domain/__init__.py
@@ -2,10 +2,13 @@
 
 from .static import BalancePathResult, analyze_balance_path
 from .dynamics import SwitchingCostResult, switching_cost_state
+from .world import MiddleWorldCertificate, classify_middle_world
 
 __all__ = [
     "BalancePathResult",
+    "MiddleWorldCertificate",
     "SwitchingCostResult",
     "analyze_balance_path",
+    "classify_middle_world",
     "switching_cost_state",
 ]
