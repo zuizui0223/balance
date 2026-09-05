b610a6500e61294504191cc68828e2243a820034
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-05 16:22:53 +0900

    Expose direct worldline path analysis API

--- balance_domain/__init__.py
+++ balance_domain/__init__.py
@@ -4,13 +4,16 @@ from .static import BalancePathResult, analyze_balance_path
 from .dynamics import SwitchingCostResult, switching_cost_state
 from .world import MiddleWorldCertificate, classify_middle_world
 from .worldlines import WorldlineComparison, compare_worldlines
+from .worldline_path import WorldlinePathResult, analyze_worldline_path
 
 __all__ = [
     "BalancePathResult",
     "MiddleWorldCertificate",
     "SwitchingCostResult",
     "WorldlineComparison",
+    "WorldlinePathResult",
     "analyze_balance_path",
+    "analyze_worldline_path",
     "classify_middle_world",
     "compare_worldlines",
     "switching_cost_state",
