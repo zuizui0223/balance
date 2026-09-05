c0e6165f148e7c7768af8b3fc150448802b8115c
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-05 16:30:20 +0900

    Expose critical-point concordance API

--- balance_domain/__init__.py
+++ balance_domain/__init__.py
@@ -5,9 +5,11 @@ from .dynamics import SwitchingCostResult, switching_cost_state
 from .world import MiddleWorldCertificate, classify_middle_world
 from .worldlines import WorldlineComparison, compare_worldlines
 from .worldline_path import WorldlinePathResult, analyze_worldline_path
+from .concordance import CriticalConcordanceResult, compare_critical_paths
 
 __all__ = [
     "BalancePathResult",
+    "CriticalConcordanceResult",
     "MiddleWorldCertificate",
     "SwitchingCostResult",
     "WorldlineComparison",
@@ -15,6 +17,7 @@ __all__ = [
     "analyze_balance_path",
     "analyze_worldline_path",
     "classify_middle_world",
+    "compare_critical_paths",
     "compare_worldlines",
     "switching_cost_state",
 ]
