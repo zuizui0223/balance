146b1da44e2d9ea78fc4f85e523e92534940ba6b
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-05 16:19:25 +0900

    Expose direct worldline comparison API

--- balance_domain/__init__.py
+++ balance_domain/__init__.py
@@ -3,12 +3,15 @@
 from .static import BalancePathResult, analyze_balance_path
 from .dynamics import SwitchingCostResult, switching_cost_state
 from .world import MiddleWorldCertificate, classify_middle_world
+from .worldlines import WorldlineComparison, compare_worldlines
 
 __all__ = [
     "BalancePathResult",
     "MiddleWorldCertificate",
     "SwitchingCostResult",
+    "WorldlineComparison",
     "analyze_balance_path",
     "classify_middle_world",
+    "compare_worldlines",
     "switching_cost_state",
 ]
