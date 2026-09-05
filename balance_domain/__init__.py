94375b16bc97db7a1eaa6dff4ec7ec2b785df7eb
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-05 23:49:09 +0900

    Expose critical-definition concordance API

--- balance_domain/__init__.py
+++ balance_domain/__init__.py
@@ -6,11 +6,20 @@ from .world import MiddleWorldCertificate, classify_middle_world
 from .worldlines import WorldlineComparison, compare_worldlines
 from .worldline_path import WorldlinePathResult, analyze_worldline_path
 from .concordance import CriticalConcordanceResult, compare_critical_paths
+from .definition_concordance import (
+    CrossingBracket,
+    DefinitionConcordance,
+    analyze_definitions,
+    compare_definition_brackets,
+    crossing_bracket,
+)
 from .receipt import Interval, MiddleWorldReceipt, classify_bounded_receipt
 
 __all__ = [
     "BalancePathResult",
     "CriticalConcordanceResult",
+    "CrossingBracket",
+    "DefinitionConcordance",
     "Interval",
     "MiddleWorldCertificate",
     "MiddleWorldReceipt",
@@ -18,10 +27,13 @@ __all__ = [
     "WorldlineComparison",
     "WorldlinePathResult",
     "analyze_balance_path",
+    "analyze_definitions",
     "analyze_worldline_path",
     "classify_bounded_receipt",
     "classify_middle_world",
     "compare_critical_paths",
+    "compare_definition_brackets",
     "compare_worldlines",
+    "crossing_bracket",
     "switching_cost_state",
 ]
